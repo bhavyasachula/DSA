217258924b6327bb580e73a3082b735c3c037633
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-19 19:52:26 +0530

    tracing the second Smallest element

--- Arrays/secondSmallestElem.cpp
+++ Arrays/secondSmallestElem.cpp
@@ -2,23 +2,23 @@
 #include<vector>
 using namespace std;
 
-vector<int> a = {1,4,5,5,56,6,4};
 
 int SecondSmallestElem(vector<int> arr){
     int smallest = arr[0];
     int secondSmallest = 0;
-
+    //       {1,4,5,5,56,6,4}
     for(int i=0 ; i<arr.size();i++){
         if(arr[i]<smallest){
             secondSmallest = smallest;
-            smallest = a[i];
+            smallest = arr[i];
         }
         else if(arr[i] > smallest && arr[i]<secondSmallest){
-            secondSmallest = a[i];
+            secondSmallest = arr[i];
         }
     };
     return secondSmallest;
 }
 int main(){
+    vector<int> a = {1,4,5,5,56,6,4};
    cout<<"Second Smallest Element:"<<SecondSmallestElem(a);
 }
