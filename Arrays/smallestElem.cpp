08baeda6b634dff8716d36cd669b2f55ff820418
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-19 19:54:00 +0530

    line edits

--- Arrays/smallestElem.cpp
+++ Arrays/smallestElem.cpp
@@ -5,9 +5,11 @@ using namespace std;
 //Find the Smallest Element from the Array
 
 int SmallestElem(vector<int> arr){
+    // {0,1,2,4,5,7,4}
+    //{4,5,7,2,1}
     int smallest = arr[0];
     for(int i = 0; i < arr.size(); i++ ){
-        if(arr[i]<smallest){  // opposite of largerest condition
+        if(arr[i]<smallest){  
             smallest = arr[i];
         }
     }
