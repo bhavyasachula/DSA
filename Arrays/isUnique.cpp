ac19033da1cc24a998a9a5e2bf01f91f86ae82d1
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-20 21:59:21 +0530

    Added n globally

--- Arrays/isUnique.cpp
+++ Arrays/isUnique.cpp
@@ -3,11 +3,11 @@
 #include <set>
 using namespace std;
 set<int> st;
-
+int n = arr.size();
 vector<int> arr = {1,1,2,2,2,4,4,4};
 
 int isUniqueBruteForce(vector<int> arr){
-    for(int i=0 ; i<arr.size();i++){
+    for(int i=0 ; i<n;i++){
         st.insert(arr[i]);
     }
     return st.size();
