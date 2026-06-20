e8006508a30a3879a2375e2ed0bf10bd03a714a2
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-20 21:59:48 +0530

    N in formal parameter

--- Arrays/isUnique.cpp
+++ Arrays/isUnique.cpp
@@ -6,7 +6,7 @@ set<int> st;
 int n = arr.size();
 vector<int> arr = {1,1,2,2,2,4,4,4};
 
-int isUniqueBruteForce(vector<int> arr){
+int isUniqueBruteForce(vector<int> arr,int n){
     for(int i=0 ; i<n;i++){
         st.insert(arr[i]);
     }
