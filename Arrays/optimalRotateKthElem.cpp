76ef7e93caf29cdc718cef094d09feedf7f50a18
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-21 23:31:12 +0530

    reversinbg the array

--- Arrays/optimalRotateKthElem.cpp
+++ Arrays/optimalRotateKthElem.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-vector<int> optimalLeftRotateByK(vector<int> arr,int n){
-    
+vector<int> optimalLeftRotateByK(vector<int> arr,int n,int k){
+    reverse(arr.begin(),arr.begin()+k);
+    reverse(arr.begin()+k,arr.begin()+n);
+    reverse(arr.begin(),arr.end());    
 }
