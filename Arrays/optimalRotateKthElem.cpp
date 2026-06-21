80376cf090970424f9aaadb7d544fee5edfe14d2
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-21 23:42:59 +0530

    Optimal array succesfully done

--- Arrays/optimalRotateKthElem.cpp
+++ Arrays/optimalRotateKthElem.cpp
@@ -3,13 +3,19 @@
 #include <algorithm>
 
 using namespace std;
-
+vector<int> arr = {,4,5,6,7,81,2};
 vector<int> optimalLeftRotateByK(vector<int> arr,int n,int k){
+    k=k%n;
     reverse(arr.begin(),arr.begin()+k);
     reverse(arr.begin()+k,arr.begin()+n);
     reverse(arr.begin(),arr.end());    
     return arr;
 }
 int main(){
+    int n = arr.size();
+    vector<int> rotatedKtharray = optimalLeftRotateByK(arr,n,2);
+    for(int x : rotatedKtharray){
+        cout<< x;
+    }
     
 }
