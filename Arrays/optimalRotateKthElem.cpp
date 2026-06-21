2e8f197537c487db27e566f286ffaa5646c7aea9
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-21 23:45:53 +0530

    Added an info for arr index bound

--- Arrays/optimalRotateKthElem.cpp
+++ Arrays/optimalRotateKthElem.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 vector<int> arr = {1,2,4,5,6,7,8};
 vector<int> optimalLeftRotateByK(vector<int> arr,int n,int k){
-
+    k=k%n; // k should not be greater than array 
     reverse(arr.begin(),arr.begin()+k);
     reverse(arr.begin()+k,arr.begin()+n);
     reverse(arr.begin(),arr.end());    
