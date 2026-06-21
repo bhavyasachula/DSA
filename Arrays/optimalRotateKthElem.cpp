ccbcbb1c06f27f5b620960abe4a2239ec6847cf2
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-21 23:45:09 +0530

    checking if error shows up or not

--- Arrays/optimalRotateKthElem.cpp
+++ Arrays/optimalRotateKthElem.cpp
@@ -3,9 +3,10 @@
 #include <algorithm>
 
 using namespace std;
+
 vector<int> arr = {1,2,4,5,6,7,8};
 vector<int> optimalLeftRotateByK(vector<int> arr,int n,int k){
-    k=k%n;
+
     reverse(arr.begin(),arr.begin()+k);
     reverse(arr.begin()+k,arr.begin()+n);
     reverse(arr.begin(),arr.end());    
