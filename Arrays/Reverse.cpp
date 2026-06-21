a34e6c78aa589dd3dfe8679b192d2330406c84a0
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-21 23:06:47 +0530

    Reverse an array Elements

---
+++ Arrays/Reverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+vector<int> arr = {1,2,4,5,6,7};
+
+vector<int> ReverseArray(vector<int> arr , int startindex , int lastindex){
+    for(int i = lastindex ; i>=startindex ; i--){
+        return arr;
+    }
+}
+
+int main(){
+    ReverseArray(arr,arr.sziue)
+}
