7eaf15c1d1d6cc692081c4199c04b7096b0882b0
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-22 22:51:13 +0530

    build a logic to union two arrays

--- Arrays/UnionOfTwoArrays.cpp
+++ Arrays/UnionOfTwoArrays.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <set>
+
 using namespace std;
 
 vector<int> arr1 = {1,2,4,4,5,6,6};
 vector<int> arr2 = {2,5,4,7,6,4};
 set<int> st;
+
+// vector<int> arr
+int n1 = arr1.size();
+int n2 = arr2.size();
+
+vector<int> UnionTwoArrays(vector<int> arr1,vector<int> arr2){
+    for(int i=0;i<n1;i++){
+        st.insert(arr1[i]);
+    }
+    for(int j=0; j<n2 ; j++){
+        st.insert(arr2[j]);
+    }
+    vector<int> unionn;
+    for(int x: st){
+        unionn.push_back(x);
+    }
+}
+
