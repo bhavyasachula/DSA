c9f986b5eabb216f51b2344bc318d325dac37ecb
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-23 00:12:48 +0530

    Optimal:Intersection of two arrays

--- Arrays/intersectionOfTwoArray.cpp
+++ Arrays/intersectionOfTwoArray.cpp
@@ -1,3 +1,35 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+//Optimal Approach
+
+vector<int> a={1,2,4,5,6,7,7,6,7}; // Array should be sorted 
+vector<int> b={1,2,4,5,7,65};
+int n=a.size();
+int m = b.size();
+vector<int> IntersectionOfTwoArrays(vector<int> arr1,vector<int> arr2){     
+    int i=0;
+    int j=0;
+    vector<int> ans;
+    while(i<n and j<m){
+        if(a[i] < b[j]){
+            i++;
+        }
+        else if(b[j] < a[i]){
+            j++;
+        }
+        else{
+            ans.push_back(a[i]);
+            i++;
+            j++;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    for(int x : IntersectionOfTwoArrays(a,b)){
+        cout<<x<<" ";
+    }
+}
