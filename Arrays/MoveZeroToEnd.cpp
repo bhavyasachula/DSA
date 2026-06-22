bf8a2521c7c4ea1833aee1e42c0156fc2f88690f
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-22 12:47:20 +0530

    BruteForce:Move Zero To End

---
+++ Arrays/MoveZeroToEnd.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//Brute Force -Move Zero To End;
+vector<int> outerarr = {1,2,4,0,0,0,7,8,2};
+
+int n=outerarr.size();
+vector<int> moveZeroTEnd(vector<int> arr,int n){
+    vector<int> temp = {};
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            temp.push_back(arr[i]);
+        }
+    }
+    int nz = temp.size();
+    for(int i = nz;i<n;i++){
+        temp.push_back(0);
+    }
+    return temp;
+}
+
+int main(){
+    
+    vector<int> arr = moveZeroTEnd(outerarr,n);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout<<arr[i];
+    }
+    
+}
