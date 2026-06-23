d891e3f9b791a79715529d8cc1839cfc0de9c215
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-23 23:29:26 +0530

    Adding an info for mapping last Nth element

--- Arrays/OptimalMissingNumber.cpp
+++ Arrays/OptimalMissingNumber.cpp
@@ -17,20 +17,21 @@ vector<int> arr = {1,2,4,5};
 // }
 int xor1=0;
 int xor2=0;
-int n = N-1;
+
 int missingNumber(vector<int> arr , int n){
 
-
+    int n = N-1;
     for(int i=0 ; i<n; i++){  
          xor1 = xor1 ^ arr[i];
-         
+        // xoring till the fourth element element  
          xor2 = xor2 ^ (i+1);
         
     }
+    //now xoring till the N=5
     xor1 = xor1 ^ N; 
     return (xor2 ^ xor1);
 }
 
 int main(){
-    cout<<"Missing Number is :"<< missingNumber(arr,n);
+    cout<<"Missing Number is :"<< missingNumber(arr,N);
 }
