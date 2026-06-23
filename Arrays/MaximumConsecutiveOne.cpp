08f8234f13e83492ee4ac2b4c3a88a2a030922ff
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-23 23:38:08 +0530

    written the boiler plate

---
+++ Arrays/MaximumConsecutiveOne.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// array of 1's and 0's elements 
+
+vector<int> arr = {1,1,0,1,1,1,0,1,1};
+
+int FindConsecutive1s(vector<int> arr){
+    int counter = 0;
+    int max = 0;
+
+    return max;
+}
+
+int main(){
+    cout << FindConsecutive1s(arr);
+}
