df0f47e1368d2ee81af6ba86e40c5c182f83899f
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-23 23:25:47 +0530

    Xor approach for missing number

--- Arrays/OptimalMissingNumber.cpp
+++ Arrays/OptimalMissingNumber.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int n=5;
+int N=5;
 vector<int> arr = {1,2,4,5};
 
+// int missingNumber(vector<int> arr , int n){
+//     int sum = (n * (n+1))/2;     
+//     int consecutiveArrSum=0;
+//     for(int i=0; i<arr.size() ; i++){ // 0,1,2,3,4
+//         consecutiveArrSum += arr[i];
+//     }
+//     return (sum - consecutiveArrSum);
+// }
+// int main(){
+//    cout<<"Missing Number from the Array:"<<missingNumber(arr,n);
+// }
+int xor1=0;
+int xor2=0;
+int n = N-1;
 int missingNumber(vector<int> arr , int n){
-    int sum = (n * (n+1))/2; 
-    
-    int consecutiveArrSum=0;
-    for(int i=0; i<arr.size() ; i++){ // 0,1,2,3,4
-        consecutiveArrSum += arr[i];
+
+
+    for(int i=0 ; i<n; i++){  
+         xor1 = xor1 ^ arr[i];
+         
+         xor2 = xor2 ^ (i+1);
+        
     }
-    
-    return (sum - consecutiveArrSum);
+    xor1 = xor1 ^ N; 
+    return (xor2 ^ xor1);
 }
 
 int main(){
-   cout<<"Missing Number from the Array:"<<missingNumber(arr,n);
+    cout<<"Missing Number is :"<< missingNumber(arr,n);
 }
