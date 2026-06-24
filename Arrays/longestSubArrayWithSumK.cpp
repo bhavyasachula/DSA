f3834123d117978bba617de36ae910d9faea5676
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-24 23:41:51 +0530

    using Two pointer approach

--- Arrays/longestSubArrayWithSumK.cpp
+++ Arrays/longestSubArrayWithSumK.cpp
@@ -5,5 +5,6 @@ using namespace std;
 vector<int> arr = {1,2,3,1,1,1,1,3,3};
 int n = arr.size();
 int longestSubArrayWithSumK(vector<int> arr,int n ){
-    
+    int left = 0;
+    int right = 0;
 }
