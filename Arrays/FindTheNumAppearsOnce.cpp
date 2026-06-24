cd9d083735232c206dccf935785a741a50ab46a7
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-24 20:08:51 +0530

    Find the number appearing once

---
+++ Arrays/FindTheNumAppearsOnce.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//Find Num Appearing Once In Array And Other Twice
+
+//Note: the array should have only elements should be in pair of two elements 
+vector<int> arr = {1,1,2,4,4,4,4,5,5};
+int n=arr.size();
+
+int FindNumAppearingOnce(vector<int> arr,int n){
+    int XOR=0;
+    for(int i=0 ; i<n ;i++){
+        XOR = XOR ^ arr[i];
+    }
+    return XOR;
+}
+
+int main(){
+    cout<<"Appearing Once from the Array is:"<<FindNumAppearingOnce(arr,n);
+}
