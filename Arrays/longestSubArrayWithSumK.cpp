bb22980dd0bb6fa0cdf0cb0a8bc1d148a1258164
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-24 23:48:48 +0530

    writing the logic of max and sum

--- Arrays/longestSubArrayWithSumK.cpp
+++ Arrays/longestSubArrayWithSumK.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 vector<int> arr = {1,2,3,1,1,1,1,3,3};
 int n = arr.size();
-int longestSubArrayWithSumK(vector<int> arr,int n ){
+long long k = 6;
+int longestSubArrayWithSumK(vector<int> arr,int n,long long k ){
     int left = 0; //left pointer 
     int right = 0; //right pointer
     long long sum = arr[0];
-    while(right<n){ //loop until the array length excedded
-
+    int maxLen=0;
+    while(right<n){ 
+    //loop until the array length excedded
+        if(sum == k){
+            maxLen = max(maxLen,right-left+1);
+        }
+        right++;
+        if(right< n)
     }
 }
