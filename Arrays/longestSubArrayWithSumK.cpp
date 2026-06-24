bca2ae2d906c3d591a08df5e05e64713178ff359
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-24 23:42:22 +0530

    Two pointer info

--- Arrays/longestSubArrayWithSumK.cpp
+++ Arrays/longestSubArrayWithSumK.cpp
@@ -5,6 +5,6 @@ using namespace std;
 vector<int> arr = {1,2,3,1,1,1,1,3,3};
 int n = arr.size();
 int longestSubArrayWithSumK(vector<int> arr,int n ){
-    int left = 0;
-    int right = 0;
+    int left = 0; //left pointer 
+    int right = 0; // right pointer
 }
