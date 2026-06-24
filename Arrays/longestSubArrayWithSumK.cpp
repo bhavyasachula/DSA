1575d495d657df8f87ff67f6a435894f3222dbf3
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-24 23:45:25 +0530

    using sum counter to identify the sum of the array

--- Arrays/longestSubArrayWithSumK.cpp
+++ Arrays/longestSubArrayWithSumK.cpp
@@ -6,8 +6,9 @@ vector<int> arr = {1,2,3,1,1,1,1,3,3};
 int n = arr.size();
 int longestSubArrayWithSumK(vector<int> arr,int n ){
     int left = 0; //left pointer 
-    int right = 0; // right pointer
-    while(right<n){
-        
+    int right = 0; //right pointer
+    long long sum = arr[0];
+    while(right<n){ //loop until the array length excedded
+
     }
 }
