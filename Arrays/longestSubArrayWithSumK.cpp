5d91f180c73b673442ced13c146c326bbdc477dd
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-25 20:02:57 +0530

    written logic for longest sub array with sum k

--- Arrays/longestSubArrayWithSumK.cpp
+++ Arrays/longestSubArrayWithSumK.cpp
@@ -10,12 +10,24 @@ int longestSubArrayWithSumK(vector<int> arr,int n,long long k ){
     int right = 0; //right pointer
     long long sum = arr[0];
     int maxLen=0;
-    while(right<n){ 
-    //loop until the array length excedded
-        if(sum == k){
-            maxLen = max(maxLen,right-left+1);
-        }
-        right++;
-        if(right< n)
+    while(right<n){ //loop until the array length excedded
+       while(left <= right && sum>k){
+        sum-=arr[left];
+        left++;
+       }
+       if(sum == k){
+         maxLen = max(maxLen, right - left + 1);
+       }
+      right++;
+      if (right < n) {// This condition ensures that the right pointer is still inside the array. Without it, we might try to access an invalid index and get an error.
+      //only inside the boundary then add
+        sum+=arr[right];
+    
     }
+    return maxLen;
+}
+}
+int main()
+{
+   cout<<"longest sub array len is :"<<longestSubArrayWithSumK(arr,n,k);
 }
