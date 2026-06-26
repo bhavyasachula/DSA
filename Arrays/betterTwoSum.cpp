658cdbc3bccbf96f1a18c3b70be713e5b99600b7
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-26 21:41:02 +0530

    Initialized the better approach to solve the two sum

---
+++ Arrays/betterTwoSum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include<map>
+using namespace std;
+
+vector<int> arr = {6,2,4,8,11};
+int n=arr.size();
+int twoSum = 14;
+
+pair<int,int> twoSums(vector<int> arr , int n){
+    map<int,int>
+}
