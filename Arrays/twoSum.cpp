032802cca3a0615cfc6287ed5567dc20545258f2
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-26 20:36:41 +0530

    Brute:Two Sum

---
+++ Arrays/twoSum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> arr = {6,2,4,8,11};
+int n=arr.size();
+int twoSum = 14;
+
+pair<int,int> twoSums(vector<int> arr , int n){
+    for(int i=0 ; i < n ; i++){
+        for(int j=i ; j < n ; j++ ){
+
+            if(arr[i] + arr[j+1] == twoSum){
+                return {i,j+1};
+            }  
+       }
+    }
+}
+
+int main(){
+    pair<int,int> returnedIndex = twoSums(arr,n);
+    cout<<"FirstIndexOfTwoSum:"<<returnedIndex.first<<"\n"<<"SecondIndexOfTwoSum:"<<returnedIndex.second;
+}
