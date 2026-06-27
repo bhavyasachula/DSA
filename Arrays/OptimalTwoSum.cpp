b9182733eba4681432119231b177900ebb15db95
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-27 12:58:54 +0530

    bit more correction

--- Arrays/OptimalTwoSum.cpp
+++ Arrays/OptimalTwoSum.cpp
@@ -8,10 +8,11 @@ vector<int> arr = {2,6,5,8,11};
 int n =  arr.size();
 int target = 14;
 string TwoSum(vector<int> arr , int n,int target){
+     int left=0;
+    int right=0;
     int sum = arr[left]+arr[right];
     sort(arr.begin(),arr.end());
-    int left=0;
-    int right=0;
+   
     while(left<right){
         
         if(sum == target){
