7dbf62514503410c11fddc76029bc580818ef6e2
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-27 20:39:00 +0530

    Revised logic

--- Arrays/OptimalTwoSum.cpp
+++ Arrays/OptimalTwoSum.cpp
@@ -7,14 +7,14 @@ using namespace std;
 vector<int> arr = {2,6,5,8,11};
 int n =  arr.size();
 int target = 14;
-string TwoSum(vector<int> arr , int n,int target){
-     int left=0;
+string TwoSum(vector<int> arr , int n ,int target){
+    int left=0;
     int right=0;
-    int sum = arr[left]+arr[right];
+    
     sort(arr.begin(),arr.end());
    
     while(left<right){
-        
+        int sum = arr[left] + arr[right];
         if(sum == target){
             return "Target is Present";
         }
