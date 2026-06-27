dde67aa2a0fb0bd64f0f7783cb654706a943776d
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-27 12:56:58 +0530

    Optimal:Two sum logic

---
+++ Arrays/OptimalTwoSum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+//Optimal
+vector<int> arr = {2,6,5,8,11};
+int n =  arr.size();
+int target = 14;
+string TwoSum(vector<int> arr , int n,int target){
+    int sum=0;
+    sort(arr.begin(),arr.end());
+    int left=0;
+    int right=0;
+    while(left<right){
+
+        if(sum == arr[left]+arr[right]){
+            return "Target is Present";
+        }
+        else if(sum < target){
+            left++;
+        }
+        else right--;
+        
+    }
+}
