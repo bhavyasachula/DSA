92590f28ba073c542cf48c555f4a05fa5af7d635
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-28 23:08:45 +0530

    Modularize the code

--- Arrays/majorityElement.cpp
+++ Arrays/majorityElement.cpp
@@ -9,11 +9,12 @@ using namespace std;
     return the element which appears n/2 times from the array
 
     */
+
+
 vector<int> arr = {1,1,1,2,2,1};
 int n=arr.size();
-int cnt ;
-int main(){
-    cnt = 0;
+int MajorityElement(vector<int> arr ,int n){
+     int cnt = 0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if(arr[i]==arr[j]){
@@ -22,4 +23,8 @@ int main(){
         }
         if(cnt>(n/2)) return arr[i];
     }
+}
+
+int main(){
+
 }
