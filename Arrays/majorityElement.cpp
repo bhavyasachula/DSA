d3955fa90288937164c8a0e3cfd65863c104bdaf
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-28 23:01:50 +0530

    More relevant info about majorityElement

--- Arrays/majorityElement.cpp
+++ Arrays/majorityElement.cpp
@@ -2,5 +2,10 @@
 #include<vector>
 using namespace std;
 /*
-    find the majority Element from the array   
-*/
+    find the majority Element from the array;
+    {1,1,1,2,2,1}
+    Output :
+    n=size of array
+    return the element which appears n/2 times from the array
+
+    */
