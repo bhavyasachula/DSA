eb02f9656f0ebe737685565e89f1076b464211ff
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-29 23:19:33 +0530

    Optimal:Using moore voting algorithm

--- Arrays/OptimalMajorityElement.cpp
+++ Arrays/OptimalMajorityElement.cpp
@@ -5,6 +5,21 @@ using namespace std;
 vector<int> arr = {1,2,3,1,1,1,1,3,3};
 int n = arr.size();
 
-vector<int> OptimalMajorityElement(vector<int> arr ,int n){
-    
+// Moore Voting algorithm
+
+vector<int> OptimalMajorityElement(vector<int> arr,int n){
+    int el=0;
+    int cnt=0;
+    for(int i=0;i<arr.size();i++){
+        if(cnt==0){
+            cnt=1;
+            el=arr[i];
+        }
+        else if(el==arr[i]){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+    }
 }
