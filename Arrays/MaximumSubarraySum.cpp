9b9754ce18ac8d1d4df4a56eea3342ace57e2f4a
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-30 15:15:58 +0530

    Better:returned both start and end index with maximum subarray

--- Arrays/MaximumSubarraySum.cpp
+++ Arrays/MaximumSubarraySum.cpp
@@ -5,26 +5,30 @@ using namespace std;
 vector<int> arr={-2,-3,4,-1,-2,1,5,-3};
 int n=arr.size();
  
-int maximumSubArray(vector<int> arr,int n){
+vector<int> maximumSubArray(vector<int> arr,int n){
 
     int maxi=INT32_MIN;
     /*this loop find every possible subarray and keep the maximum 
     sum into the max and returns the max*/
-    
+    int start=0;
+    int end=0;
     for(int i=0;i<n;i++){
         int sum=0;
         for(int j=i ; j<n ; j++){
             sum=sum+arr[j];
             if(sum>maxi){
                 maxi=sum;
-                int start=i;
-                int end=j;
+                start=i;
+                end=j;
             }
 
         }
     }
-    return maxi; 
+    return {maxi,start,end}; 
 }
 int main(){
-    cout<<"Maximum Subarray sum is:"<<maximumSubArray(arr,n);
+    vector<int> x=maximumSubArray(arr,n);
+    cout<<"Maximum Sum in Subarray :"<<x[0]<<"\n";
+    cout<<"Starting Index :"<<x[1]<<"\n";
+    cout<<"Ending Index :"<<x[2];
 }
