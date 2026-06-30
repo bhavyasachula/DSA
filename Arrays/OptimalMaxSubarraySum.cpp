aaf8f62665e37838871396026c7ad6766a6da964
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-30 14:55:49 +0530

    Optimal:Using kadane Algorithm

--- Arrays/OptimalMaxSubarraySum.cpp
+++ Arrays/OptimalMaxSubarraySum.cpp
@@ -6,7 +6,19 @@ vector<int> arr={-2,-3,4,-1,-2,1,5,-3};
 int n=arr.size();
 
 int OptimalMaximumSubArraySum(vector<int> arr,int n){
+    int sum=0;
+    int maxi=0;
     for(int i=0 ; i<n ;i++){
-        
+        sum=sum+arr[i];
+        if(sum>maxi){
+            maxi=sum;
+        }
+        if(sum<0){
+            sum=0;
+        }
+        if(maxi<0){
+            maxi=0;
+        }
     }    
+    return max;
 }
