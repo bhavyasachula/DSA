830d6d13b3b0c3efcd3abac7637a34e238bbf4a6
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-30 14:37:29 +0530

    discovering the maximum sum of subarray

--- Arrays/MaximumSubarraySum.cpp
+++ Arrays/MaximumSubarraySum.cpp
@@ -2,3 +2,22 @@
 #include <vector>
 using namespace std;
 
+vector<int> arr={-2,-3,4,-1,-2,1,5,3};
+int n=arr.size();
+
+int maximumSubArray(vector<int> arr,int n){
+    int maxi=INT32_MIN;
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i ; j<n ; j++){
+            sum=sum+arr[j];
+            if(sum>maxi){
+                maxi=sum;
+                int start=i;
+                int end=j;
+            }
+        }
+
+    }
+    return 
+}
