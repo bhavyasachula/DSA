17d5309ba8c7195a7f034dad7e8831e8200c42df
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-30 15:01:26 +0530

    Adding Condition Info

--- Arrays/OptimalMaxSubarraySum.cpp
+++ Arrays/OptimalMaxSubarraySum.cpp
@@ -10,12 +10,21 @@ int OptimalMaximumSubArraySum(vector<int> arr,int n){
     int maxi=0;
     for(int i=0 ; i<n ;i++){
         sum=sum+arr[i];
+        
         if(sum>maxi){
             maxi=sum;
         }
+        /*
+        if sum encounters any negative do not consider it 
+        and reinitialize the sum value to 0
+        */
         if(sum<0){
             sum=0;
         }
+        /*
+        if the subarray contains all the negative or 
+        lessthan zero element then just return the zero not negative values
+        */
         if(maxi<0){
             maxi=0;
         }
