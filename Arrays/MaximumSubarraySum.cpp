9fa62e75cb69e6720642dfd89e4aa17dc4157242
bhavya sachula <bhavyasachula07@gmail.com>
2026-06-30 14:43:53 +0530

    Better:Completed Better approach of maximum subarray sum

--- Arrays/MaximumSubarraySum.cpp
+++ Arrays/MaximumSubarraySum.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-vector<int> arr={-2,-3,4,-1,-2,1,5,3};
+vector<int> arr={-2,-3,4,-1,-2,1,5,-3};
 int n=arr.size();
  
 int maximumSubArray(vector<int> arr,int n){
@@ -25,4 +25,6 @@ int maximumSubArray(vector<int> arr,int n){
     }
     return maxi; 
 }
-int main()
+int main(){
+    cout<<"Maximum Subarray sum is:"<<maximumSubArray(arr,n);
+}
