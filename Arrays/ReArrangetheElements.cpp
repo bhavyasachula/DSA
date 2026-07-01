56afecc5997231c3669196238ec2594ca99c5adc
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-01 23:30:45 +0530

    OptimalReArrangeElement:Execution completed Succesfully

---
+++ Arrays/ReArrangetheElements.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> arr = {4,1,-2,-5,2,-4};
+int n=arr.size();
+
+vector<int> ReArrangeElements(vector<int> arr , int n){
+    vector<int> reArrange(n,0);
+    int posIndex=0;
+    int negIndex=1;
+    for(int i=0 ; i<n+1 ; i++){
+        
+        if(arr[i]>0){
+            reArrange[posIndex] = arr[i];
+            posIndex=posIndex+2;
+        }
+        else{
+            reArrange[negIndex] = arr[i];
+            negIndex+=2;
+        }
+    }
+    return reArrange;
+}
+int main(){
+    
+    for(int it:ReArrangeElements(arr,n)){
+        cout<<it<<" ";
+    }
+    
+}
