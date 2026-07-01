93c913fbc073e2ba42ccd458fc61ec8f0a44368f
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-01 22:52:52 +0530

    Optimal:made into structured format

--- Arrays/buyAndSellStock.cpp
+++ Arrays/buyAndSellStock.cpp
@@ -5,8 +5,8 @@ using namespace std;
 vector<int> arr ={7,1,3,4,6};
 int n=arr.size();
 
-int main(){
-    int mini=arr[0];
+int BuyAndSellStocks(vector<int> arr , int n){
+     int mini=arr[0];
     int profit=0;
     int cost;
     for(int i=1;i<n;i++){
@@ -14,5 +14,8 @@ int main(){
         profit = max(profit,cost);
         mini=min(mini,arr[i]);
     }   
-    return mini;
+    return profit;
+}
+int main(){
+   
 }
