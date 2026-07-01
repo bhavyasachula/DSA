abf1e7d4700b0265ed311deb429ae482ebf7d365
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-01 22:49:58 +0530

    Optimal:written the logic of the buyAndSellstocks

--- Arrays/buyAndSellStock.cpp
+++ Arrays/buyAndSellStock.cpp
@@ -6,5 +6,13 @@ vector<int> arr ={7,1,3,4,6};
 int n=arr.size();
 
 int main(){
- 
+    int mini=arr[0];
+    int profit=0;
+    int cost;
+    for(int i=1;i<n;i++){
+        cost = arr[i] - mini;
+        profit = max(profit,cost);
+        mini=min(mini,arr[i]);
+    }   
+    return mini;
 }
