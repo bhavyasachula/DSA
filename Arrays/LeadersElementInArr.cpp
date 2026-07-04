1aba5db6bb3170acc93b256641d0c13abc645bcd
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-04 14:50:24 +0530

    initialized the leadersElements

---
+++ Arrays/LeadersElementInArr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> arr = {1,1,1,4,4,4,2,2};
+int n = arr.size();
+vector<int> LeaderElements(vector<int> arr , int n){
+
+}
+
+int main(){
+    LeaderElements(arr,n);
+}
