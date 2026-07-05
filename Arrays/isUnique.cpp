580d2f3959adb9e98f9ed34e4f69699b3fcd69ca
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-05 23:39:16 +0530

    Optimal:isUnique elements without set dataStruct

--- Arrays/isUnique.cpp
+++ Arrays/isUnique.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <set>
 using namespace std;
-set<int> st;
-int n = arr.size();
-vector<int> arr = {1,1,2,2,2,4,4,4};
 
+
+vector<int> arr = {1,1,2,2,2,4,4,4};
+int n = arr.size();
+/*
+    Optimal Checking is unique and making unique array elements
+*/
+int insertIndex = 1;
 int isUniqueBruteForce(vector<int> arr,int n){
-    for(int i=0 ; i<n;i++){
-        st.insert(arr[i]);
+    for(int i=1 ; i<n;i++){
+        if(arr[i]!=arr[i-1]){
+            arr[insertIndex] = arr[i];
+            insertIndex++; 
+        }
     }
-    return st.size();
+    return insertIndex;
 }
 
 int main(){
---
+++ Arrays/pattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n=6;
+int main(){
+    for(int i=1;i<5;i++){
+        for(int j=1;j<n-3;j++){
+            cout<<"* ";
+        }
+        
+        cout<<"\n"; 
+    }
+}
