025fbed7dadedffff0e61d1218bc69ba87bced06
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-08 23:58:56 +0530

    info section

--- Arrays/setmatrizZero.cpp
+++ Arrays/setmatrizZero.cpp
@@ -7,6 +7,7 @@ int n=3;
 int m=3;
 vector<vector<int>> setMatrixZero(vector<vector<int>> arr , int n,int m){
     vector<bool> rows(n, false), cols(m, false);
+    //info here
     for(int i=0; i<n ; i++){
         for(int j=0 ; j<m ; j++){
             if(arr[i][j] == 0){
