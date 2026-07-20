5b6c816e35cdda5dfe3a2351dd6847932765465a
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-20 22:31:17 +0530

    1 dimension looping

--- Arrays/SetMatrixZero.cpp
+++ Arrays/SetMatrixZero.cpp
@@ -8,4 +8,8 @@ vector<vector<int>> arr =  {{1,1,1},{1,0,1},{1,1,1}};
 int n = 3;
 int m = 3;
 
-vector<int>> SetMatrixZero(vector<vector<int>>)
+vector<vector<int>> SetMatrixZero(vector<vector<int>>){
+    for(int i =0;i<n;i++){
+        g
+    }
+}
