4bccb4798d19ad9f64e3d20956af5a37b5895189
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-20 22:13:55 +0530

    Reverse the order of whole word

--- Arrays/ReverseWordString.py
+++ Arrays/ReverseWordString.py
@@ -1,9 +1,5 @@
 s = "the sky is blue";
-output = ""
-
-output = s.split();
-print(output)
-
+output = s.split(); # first start with removing the empty space and adding into the list
 result = " ".join(output[::-1])
-
+# reverse using slicing and then join with space
 print(result)
