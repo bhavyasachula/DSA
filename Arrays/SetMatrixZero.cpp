c5ee956a808c94859013b22b8e0516dd4bee2026
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-22 23:52:58 +0530

    Core concept

--- Arrays/SetMatrixZero.cpp
+++ Arrays/SetMatrixZero.cpp
@@ -5,13 +5,16 @@ using namespace std;
 
 vector<vector<int>> arr =  {{1,1,1},{1,0,1},{1,1,1}};
 
+The Core Idea
+
+Instead of allocating separate col[] and row[] arrays, it hijacks matrix[0][j] and matrix[i][0] as markers. The only edge case: matrix[0][0] can't serve both the first row AND first column, so col0 handles the first column separately.
 int n = 3;
 int m = 3;
 
 vector<vector<int>> SetMatrixZero(vector<vector<int>>){
-    for(int i =0;i<n;i++){
+    for(int i = 0;i<n;i++){
         for(int j=0;j<m;j++){
-            
+          
         }
     }
 }
