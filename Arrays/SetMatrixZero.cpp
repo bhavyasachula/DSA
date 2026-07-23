fb2069be19e86bab5d6303aa899af1241934d365
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-23 23:40:33 +0530

    check:edge cases

--- Arrays/SetMatrixZero.cpp
+++ Arrays/SetMatrixZero.cpp
@@ -10,12 +10,15 @@ vector<vector<int>> matrix =  {{1,1,1},{1,0,1},{1,1,1}};
 // Instead of allocating separate col[] and row[] arrays, it hijacks matrix[0][j] and matrix[i][0] as markers. The only edge case: matrix[0][0] can't serve both the first row AND first column, so col0 handles the first column separately.
 int n = 3;
 int m = 3;
-
+int col0 = 1;
 vector<vector<int>> SetMatrixZero(vector<vector<int>> martix){
     for(int i = 0;i<n;i++){
         for(int j=0;j<m;j++){
             if(matrix[i][j] == 0){
                 matrix[i][0] = 0;
+                if(j!=0){
+                    matrix[0][j] = 0;
+                }
             }
         }
     }
