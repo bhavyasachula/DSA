1b00d4f9bd6fbac7c2f1188503015b24132eed10
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-26 23:39:19 +0530

    Passed the iterators

--- Arrays/rotateMatrix90Deg.cpp
+++ Arrays/rotateMatrix90Deg.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-int rotateMatrixBy90(vector<vector<int>> matrix){
+int rotateMatrixBy90(vector<vector<int>> matrix , int n , int m){
         for(int i = 0;i<n;i++){
                for(int j=0;j<m;j++){
 
