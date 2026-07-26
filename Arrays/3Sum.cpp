bf38fe67bcae84b74efadc981ad5da6e1c4b38aa
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-26 23:50:54 +0530

    edit:arr size

--- Arrays/3Sum.cpp
+++ Arrays/3Sum.cpp
@@ -2,6 +2,11 @@
 #include <vector>
 using namespace std;
 
+vector<int> arr = {-1,1,2,7,6,4};
+int n = arr.size();
 int ThreeSum(vector<int> arr){
     
+}
+int main(){
+    int ans = ThreeSum(arr);
 }
