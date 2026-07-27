0e6bc426d2a7f159bd0ef4f5b2b2a561270b2f3f
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-27 23:31:21 +0530

    MiddleLoop j

--- Arrays/3Sum.cpp
+++ Arrays/3Sum.cpp
@@ -6,7 +6,9 @@ vector<int> arr = {-1,1,2,7,6,4};
 int n = arr.size();
 int ThreeSum(vector<int> arr,int n){
     for(int i=0;i<n ; i++){
-        
+        for(int j=i+1;j<n ;j++){
+            
+        }
     }
 }
 
