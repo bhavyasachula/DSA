fc8462228229791c35f0c8f2fb2e567dd3982d61
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-27 23:33:42 +0530

    Added a condition for three sum

--- Arrays/3Sum.cpp
+++ Arrays/3Sum.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 
 vector<int> arr = {-1,1,2,7,6,4};
+set<vector<int>> st;
 int n = arr.size();
 int ThreeSum(vector<int> arr,int n){
-    for(int i=0;i<n ; i++){
-        for(int j=i+1;j<n ;j++){
-            
+    for(int i=0; i<n ; i++){
+        for(int j=i+1 ; j<n ; j++){
+            for(int k=j+1 ; k<n ; k++){
+                if(arr[i] + arr[j] + arr[k] == 0){
+
+                }
+            }
         }
     }
 }
