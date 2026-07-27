cab911f6e68373902cb532038deeda7b395ed1fe
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-27 23:30:48 +0530

    Outer loop for i

--- Arrays/3Sum.cpp
+++ Arrays/3Sum.cpp
@@ -5,8 +5,11 @@ using namespace std;
 vector<int> arr = {-1,1,2,7,6,4};
 int n = arr.size();
 int ThreeSum(vector<int> arr,int n){
-
+    for(int i=0;i<n ; i++){
+        
+    }
 }
+
 int main(){
     int ans = ThreeSum(arr,n);
 }
