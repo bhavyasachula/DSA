58fe7970b91b30ef18de9ab5536b358cf65a2e03
bhavya sachula <bhavyasachula07@gmail.com>
2026-07-28 21:41:42 +0530

    better : Added function structure

--- Arrays/better3Sum.cpp
+++ Arrays/better3Sum.cpp
@@ -9,3 +9,5 @@ brute approach: n^3
 Better approach: n^2  
     First we need to remove the third loop that we are using in brute force approach
 */
+
+vector<vector<int>> Better3Sum(vector<int> arr)
