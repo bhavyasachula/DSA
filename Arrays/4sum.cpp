747b0c7b573d5e2c2b53d2a05456b5777ee93082
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-02 22:27:21 +0530

    approach:time complexity

--- Arrays/4sum.cpp
+++ Arrays/4sum.cpp
@@ -5,6 +5,7 @@ using namespace std;
 vector<int> arr = {-1, 0,1,1, 2, -1, -4};
 //4sum : find all unique quadruplets in an array that add up to a target.
 //Approach:Sort + two outer loops + two pointers inside
+//time complexity : O(n^3)
 vector<vector<int>> FourSum(vector<int> arr){
 
     
