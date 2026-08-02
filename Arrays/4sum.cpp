c2a3ce80587816338938c596b25025a87695d3f4
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-02 22:25:46 +0530

    Approach:two pointers

--- Arrays/4sum.cpp
+++ Arrays/4sum.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 vector<int> arr = {-1, 0,1,1, 2, -1, -4};
 //4sum : find all unique quadruplets in an array that add up to a target.
+//Approach:
 vector<vector<int>> FourSum(vector<int> arr){
 
     
