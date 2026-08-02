1c08f5c896599382e8f60f5454057b2e97fcc75e
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-02 22:31:35 +0530

    intialized the arr size

--- Arrays/4sum.cpp
+++ Arrays/4sum.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <set>
 using namespace std;
-vector<int> arr = {-1, 0,1,1, 2, -1, -4};
+vector<int> arr = {-1, 0, 1, 1, 2, -1, -4};
+int n = arr.size();
 //4sum : find all unique quadruplets in an array that add up to a target.
 //Approach:Sort + two outer loops + two pointers inside
 //time complexity : O(n^3)
