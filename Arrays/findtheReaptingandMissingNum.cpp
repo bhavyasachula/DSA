475fc9d86b93984e01a7cfce8afd2df1e26cb90f
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-05 23:16:10 +0530

    Struct:added info

--- Arrays/findtheReaptingandMissingNum.cpp
+++ Arrays/findtheReaptingandMissingNum.cpp
@@ -4,5 +4,5 @@ using namespace std;
 vector<int> arr = {1,1,2,4,4,4,4,5,5};
 int n= arr.size();
 vector<int> findRepeatingAndMissingNum(vector<int> arr,int n){
-    
+    //find repeating and missing num
 }
