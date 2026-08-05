6ac49c7be35e2162c4cad40f9720e27111bd503b
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-05 23:15:06 +0530

    Added n into formal params

--- Arrays/findtheReaptingandMissingNum.cpp
+++ Arrays/findtheReaptingandMissingNum.cpp
@@ -3,4 +3,6 @@
 using namespace std;
 vector<int> arr = {1,1,2,4,4,4,4,5,5};
 int n= arr.size();
-vector<int> findRepeatingAndMissingNum(vector<int> arr)
+vector<int> findRepeatingAndMissingNum(vector<int> arr,int n){
+    
+}
