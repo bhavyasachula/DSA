5120e68dfacc20126bb933d800754d5230aad2ba
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-11 21:57:04 +0530

    moving further

--- Arrays/3sumDiff.cpp
+++ Arrays/3sumDiff.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <algorithm>
 using namespace std;
 
 vector<int> arr = {-1, 0, 1, 1, 2, -1, -4};
 int n =  arr.size();
+sort(arr.begin(), arr.end());
+
 set<vector<int>> result ;
 vector<vector<int>> Diff3sum(vector<vector<int>> arr, int n){
     for(int i=0 ; i < n ; i++){
