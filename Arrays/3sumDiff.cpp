58e7fcb9d67a56bb8d11270b8315e35eece62520
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-11 21:52:48 +0530

    Approaching 3 sum diff

--- Arrays/3sumDiff.cpp
+++ Arrays/3sumDiff.cpp
@@ -5,12 +5,14 @@ using namespace std;
 
 vector<int> arr = {-1, 0, 1, 1, 2, -1, -4};
 int n =  arr.size();
+set<vector<int>> result ;
+vector<vector<int>> Diff3sum(vector<vector<int>> arr, int n){
+    for(int i=0 ; i < n ; i++){
 
-vector<vector<int>> Diff3sum(vector<int> arr , int n){
-
+    }
     return arr;
 }
 
 int main(){
-
+    
 }
