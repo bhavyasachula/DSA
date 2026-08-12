b8422b7c912e3646bb1f20a9d554e5458ee93bd4
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-12 23:17:48 +0530

    3sum works on flat array

--- Arrays/3sumDiff.cpp
+++ Arrays/3sumDiff.cpp
@@ -8,7 +8,7 @@ vector<int> arr = {-1, 0, 1, 1, 2, -1, -4};
 int n =  arr.size();
 
 set<vector<int>> result ;
-vector<vector<int>> Diff3sum(vector<vector<int>> arr, int n){
+vector<vector<int>> Diff3sum(vector<int> arr, int n){
     sort(arr.begin(), arr.end());
     for(int i=0 ; i < n-2 ; i++){
         int left = i+1;
