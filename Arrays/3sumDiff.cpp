296790eb3423601869a7d5bafe6280ec695fad2e
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-12 23:13:24 +0530

    Starting 3sum diff

--- Arrays/3sumDiff.cpp
+++ Arrays/3sumDiff.cpp
@@ -13,14 +13,17 @@ vector<vector<int>> Diff3sum(vector<vector<int>> arr, int n){
     for(int i=0 ; i < n-2 ; i++){
         int left = i+1;
         int right = arr.size()-1;
+
         while(left<right){
+
         int sum = arr[i] + arr[left] + arr[right];
-            if(sum == 0){
+
+            if(sum == 0){ 
                 result.insert(arr);
             }
         }
     }
-    return arr;
+    return result;
 }
 
 int main(){
