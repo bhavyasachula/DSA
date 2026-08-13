eeaa68db1087b3261b4da3313c2ed4320cba67d7
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-13 21:31:31 +0530

    info : conversion

--- Arrays/3sumDiff.cpp
+++ Arrays/3sumDiff.cpp
@@ -37,7 +37,7 @@ vector<vector<int>> Diff3sum(vector<int> arr, int n){
             }
         }
     }
-    vector<vector<int>> myanswer(result.begin(),result.end());
+    vector<vector<int>> myanswer(result.begin(),result.end()); /**/
     return myanswer;
 }
 
