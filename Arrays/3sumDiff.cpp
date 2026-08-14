f489e0c489bf5688a09f708c604d0baf935ac404
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-14 21:39:15 +0530

    return: statement

--- Arrays/3sumDiff.cpp
+++ Arrays/3sumDiff.cpp
@@ -38,7 +38,7 @@ vector<vector<int>> Diff3sum(vector<int> arr, int n){
         }
     }
     vector<vector<int>> myanswer(result.begin(),result.end()); /*Converting the set into vector of vector because of the return statement*/
-    return myanswer; /**/
+    return myanswer; /*returned */
 }
 
 int main(){
