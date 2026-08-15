a18614855b25db778ccce70ecb1218c07e97beca
bhavya sachula <bhavyasachula07@gmail.com>
2026-08-15 22:05:11 +0530

    Completed the sort color problem

--- Arrays/SortColor.cpp
+++ Arrays/SortColor.cpp
@@ -8,6 +8,7 @@ using namespace std;
 //Dutch national flag
 vector<int> nums = {2,0,2,1,1,0};
 int n = nums.size();
+
 void SortColor(vector<int> nums,int n){
 int start = 0;
 int midptr = 0;
@@ -28,5 +29,11 @@ while(midptr <= end){
             break;
     }
 }
+for(auto &res:nums){
+    cout<<res<<" ";
+   }
+}
 
+int main(){
+    SortColor(nums,n);
 }
